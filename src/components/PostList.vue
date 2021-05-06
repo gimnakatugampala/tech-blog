9e93572db2bc8ace102f49fa4cd6efaab1d7f45e
Gimna Katugampala <gimnakatugampala1@gmail.com>
2021-05-06 20:30:52 +0530

    added some hanges

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="card">
     <h5 class="card-header">Featured</h5>
-  <div class="card-body">
+    <div class="card-body">
     <h5 class="card-title">Special title treatment</h5>
     <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
     <a href="#" class="btn btn-primary">Go somewhere</a>
