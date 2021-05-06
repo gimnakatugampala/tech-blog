0da41761265f074d23588a02a754ac9de2b8e748
Gimna Katugampala <gimnakatugampala1@gmail.com>
2021-05-06 20:21:29 +0530

    added all the components PostList , Filter, Navbar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <Navbar />
+   <div class="container mt-5">
+     <Filter />
+     <PostList />
+   </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Navbar from '@/components/Navbar.vue'
+import Filter from '@/components/Filter.vue'
+import PostList from '@/components/PostList.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Navbar,
+    Filter,
+    PostList
   }
 }
 </script>
